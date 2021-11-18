f05b40b69a40cc4ca41e88856c8f58f1b860890b
Serry Saurav <sauravkumar080999@gmail.com>
2021-11-19 02:07:28 +0530

    google autosearch added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,13 @@ import React from "react";
 import { StyleSheet, Image, View, SafeAreaView, StatusBar } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={(styles.container, tw`bg-white h-full`)}>
       <View style={tw`p-5`}>
@@ -18,6 +23,37 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="from where"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: "AIzaSyBH1ihlvEFo3O1d2ht7-PBjjtg5LrwIGj4",
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
@@ -32,3 +68,5 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
   },
 });
+
+//expo r -c
